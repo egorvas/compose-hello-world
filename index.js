6774d109c325595db895e273762bb24d64bc674a
egorvas <egorvas@gmail.com>
2022-11-23 16:46:52 +0100

    Update compose with docker

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', function(req, res){
-    res.send(`${process.env.TEST_ENV||"No ENV"}: Hello wrld!`);
+    res.send(`${process.env.TEST_ENV||"No  ENV"}: Hello wrld!`);
 });
 
 app.listen(3000);
